af5e6c144fa1ce6af805b2752314e7585c4d4509
Nina Kuhlen <ninakn78@gmail.com>
2024-01-24 21:01:44 +0100

    Eroded black spots on bricks

--- brick_counter.m
+++ brick_counter.m
@@ -1,7 +1,7 @@
 img_rgb = imread(".\img_duplo.png"); % Liest das RGB-Bild von der Festplatte
 img_hsv = rgb2hsv(img_rgb);
-img_bw = rgb2gray(img_rgb); % Konvertiert das RGB-Bild in ein Graustufenbild
-img_binarized = imbinarize(img_bw);
+% img_bw = rgb2gray(img_rgb); % Konvertiert das RGB-Bild in ein Graustufenbild
+% img_binarized = imbinarize(img_bw);
 
 % HSV-Kanäle extrahieren
 hue = img_hsv(:,:,1);        % Hue-Kanal
@@ -24,8 +24,22 @@ img_filter(~mask) = 0; % Für den roten Kanal
 img_filter(~mask, :, 2) = 0; % Für den grünen Kanal
 img_filter(~mask, :, 3) = 0; % Für den blauen Kanal
 
- figure, imshow(img_rgb), title('normal') % Zeigt das Original-RGB-Bild in einem neuen Fenster an
-% figure, imshow(img_bw), title('bw') % Zeigt das Graustufenbild in einem neuen Fenster an
-% figure, imshow(img_binarized), title('binarized') % Zeigt das Graustufenbild in einem neuen Fenster an
-imshow(mask), title('mask')
-% figure, imshow(img_filter), title('filtered') % Zeigt das FIlterbild in einem neuen Fenster an
+% jetzt die Nubsis entfernen
+eroded_mask = bwareaopen(~mask, 100);
+
+% Anwenden der bereinigten Maske auf das Originalbild
+img_filter_cleaned = img_rgb;
+img_filter_cleaned(repmat(eroded_mask, [1, 1, 3])) = 0;
+
+% Bereinigte Maske auf das Originalbild anwenden
+% img_filter_cleaned = img_rgb;
+% img_filter_cleaned(~eroded_mask) = 0; % Für den roten Kanal
+% img_filter_cleaned(~eroded_mask, :, 2) = 0; % Für den grünen Kanal
+% img_filter_cleaned(~eroded_mask, :, 3) = 0; % Für den blauen Kanal
+
+% --- imshow --- %
+
+figure, imshow(img_rgb), title('normal') % Zeigt das Original-RGB-Bild an
+figure, imshow(mask), title('mask') % Zeigt die ursprüngliche Maske an
+figure, imshow(eroded_mask), title('eroded') % Zeigt die bereinigte Maske an
+figure, imshow(img_filter_cleaned), title('cleaned filtered') % Zeigt das gereinigte Filterbild an
